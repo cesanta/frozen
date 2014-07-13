338d508e5aae7e36addb29bd5edfb2ea6be2e3e8
Kira-sempai <kira-sempai@narod.ru>
2014-07-13 13:35:08 +0400

    Moved buffer size check after the declarations block Otherwise it is not a valid C code.

--- frozen.c
+++ frozen.c
@@ -360,8 +360,9 @@ int json_emit_double(char *buf, int buf_len, double value) {
 }
 
 int json_emit_quoted_str(char *buf, int buf_len, const char *str) {
-  if (buf_len <= 1) return 0;
   int i = 0, j = 0, ch;
+
+  if (buf_len <= 1) return 0;
 
 #define EMIT(x) do { if (j < buf_len) buf[j++] = x; } while (0)
 
